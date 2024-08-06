e32c40080001a636a6f7188f2999345023245d4b
GustavoDelonzek <gustavo2016delonzek@gmail.com>
2024-08-06 20:23:57 -0300

    aula

---
+++ src/controllers/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+    require_once '../chamado/src/models/User.php';
+
+    class LoginController{
+            private $usuarios;
+            
+            public function __constructor(){
+                $this->usuarios = [
+                    new User(1, 'adm@teste.com', '1234', 1),
+                    new User(2, 'normal@teste.com', '4321', 2),
+
+                ];
+            }
+
+            public function autenticar($email, $senha){
+                foreach($this->usuarios as $user){
+                    if ($user->email == $email && $user-->verificarSenha($senha)){
+                        $_SESSION['autenticacao'] = 'SIM';
+                        $_SESSION['id'] = $user->id;
+                        $_SESSION['tipo'] = $user->tipo;
+
+                        header('Location: ../chamado/src/view/home.php');
+                        exit;
+                }
+            }
+            $_SESSION['autenticacao'] = 'NÃO';
+            header('Location: index.php?login=erro');
+            exit;
+            }
+    }
+?>
